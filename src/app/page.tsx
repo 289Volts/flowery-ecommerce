1c6fdf876bf15c75caa26f1d5728af9777f86ebd
289volts <289volts@gmail.com>
2023-10-15 01:45:00 +0100

    FEAT
    Added button component with variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Button from '@/components/ui/Button';
 import Image from 'next/image';
 
 export default function Home() {
 	return (
 		<main className="">
 			<h1 className="text-desktopHeading1">Hello world</h1>
+			<Button
+				variant="primary"
+				className=""
+			>
+				Hello
+			</Button>
 		</main>
 	);
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
 import { tv, type VariantProps } from 'tailwind-variants';
-import Image from 'next/image';
 
 const button = tv({
-	base: 'border-none py-4 px-6 flex gap-2 items-center',
+	base: 'border-none py-4 px-6 flex gap-2 items-center text-mobileButton md:text-desktopButton uppercase',
 	variants: {
 		variant: {
 			primary: 'bg-black text-white border-none',
-			secondary: 'bg-transparent text-black border border-black',
+			secondary: 'bg-white text-black border border-black',
 			tertiary: 'bg-black border-white text-white border'
-		},
-		size: {
-			default: 'h-10 py-2 px-4',
-			sm: 'h-9 px-3 rounded-md'
 		}
+	},
+	compoundVariants: [
+		{
+			variant: 'primary',
+			className:
+				'md:hover:bg-gray-dark md:hover:text-white md:active:bg-black md:active:text-white md:disabled:cursor-not-allowed disabled:bg-gray disabled:text-gray-light md:focus-visible:outline-2 md:focus-visible:outline-dotted md:focus-visible:outline-offset-[2px] md:focus-visible:ring-black'
+		},
+		{
+			variant: 'secondary',
+			className:
+				'md:hover:bg-black md:hover:text-white md:active:bg-white md:active:text-black md:cursor-not-allowed disabled:border-gray-light disabled:text-gray'
+		},
+		{
+			variant: 'tertiary',
+			className:
+				'md:hover:bg-white md:hover:text-gray-dark md:active:bg-white md:active:text-black md:cursor-not-allowed disabled:bg-black disabled:text-gray-light disabled:border-gray-light'
+		}
+	],
+	defaultVariants: {
+		variant: 'primary',
 	}
 });
 
 type ButtonVariants = VariantProps<typeof button>;
 
 interface ButtonProps extends ButtonVariants {
-	children: React.ReactNode;
+	children?: React.ReactNode;
+	className?: string;
 }
-const Button: React.FC<ButtonProps> = ({ children }) => {
-	return <button>{children}</button>;
+const Button: React.FC<ButtonProps> = ({ children, className, ...props }) => {
+	return <button className={cn(button(props), className)}>{children}</button>;
 };
 
 export default Button;
