a3fde8a1c3953daf8ff9d74339bc05d53f902196
289volts <289volts@gmail.com>
2024-01-17 21:36:54 +0100

    Fix
    Fixed padding for footer sections on tablet screens and above

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,8 +9,8 @@ import Button from '../ui/Button';
 
 const Footer = () => {
 	return (
-		<footer className="border-t border-t-black divide-x divide-x-black divide-y divide-black md:grid grid-cols-2">
-			<div className="px-4 py-10 space-y-4">
+		<footer className="border-t border-t-primary divide-x divide-x-primary divide-y divide-primary md:grid grid-cols-2 xl:grid-cols-4">
+			<div className="px-4 py-10 md:px-10 space-y-4 text-pretty">
 				<p className="text-mobileBody">
 					Remember to offer beautiful flowers from Kyiv LuxeBouquets Valentines Day, Mothers Day, Christmas...
 					<br /> Reminds you 7 days before. No spam or sharing your address
@@ -20,7 +20,7 @@ const Footer = () => {
 					name="email"
 					placeholder="Your Email"
 					id=""
-					className="px-4 py-2 h-[48px] border border-gray-light bg-white placeholder:text-gray placeholder:text-sm w-full md:mt-6"
+					className="px-4 py-2 h-[48px] border border-gray-light bg-white placeholder:text-gray-dark placeholder:text-sm w-full md:mt-6"
 				/>
 				<Button
 					variant="primary"
@@ -29,23 +29,23 @@ const Footer = () => {
 					REMIND
 				</Button>
 			</div>
-			<div className="px-4 py-10 space-y-4 md:space-y-6 md:!border-t-0">
-				<h2 className="text-mobileHeading5 text-gray md:text-desktopSubtitle">Contact Us</h2>
+			<div className="px-4 py-10 md:px-10 space-y-4 md:space-y-6 md:!border-t-0">
+				<h2 className="text-mobileHeading5 text-gray-dark md:text-desktopSubtitle">Contact Us</h2>
 				<div className="space-y-2">
-					<h3 className="text-mobileCaption text-gray md:text-desktopCaption">Address</h3>
+					<h3 className="text-mobileCaption text-gray-dark md:text-desktopCaption">Address</h3>
 					<p className="text-mobileLink md:text-desktopLink">15/4 Khreshchatyk Street, Kyiv.</p>
 				</div>
 				<div className="space-y-2">
-					<h3 className="text-mobileCaption text-gray md:text-desktopCaption">Phone</h3>
+					<h3 className="text-mobileCaption text-gray-dark md:text-desktopCaption">Phone</h3>
 					<p className="text-mobileLink md:text-desktopLink">+380980099777</p>
 				</div>
 				<div className="space-y-2">
-					<h3 className="text-mobileCaption text-gray md:text-desktopCaption">General Enquiry</h3>
-					<p className="text-mobileLink md:text-desktopLink">Kiev.Florist.Studio@gmail.com</p>
+					<h3 className="text-mobileCaption text-gray-dark md:text-desktopCaption">General Enquiry</h3>
+					<p className="text-mobileLink md:text-desktopLink break-all">Kiev.Florist.Studio@gmail.com</p>
 				</div>
 				<div className="space-y-4">
-					<h3 className="text-mobileCaption text-gray">Follow Us</h3>
-					<div className="flex items-center gap-8">
+					<h3 className="text-mobileCaption text-gray-dark">Follow Us</h3>
+					<div className="flex flex-wrap items-center gap-x-8 gap-y-4">
 						<Image
 							src={instagram}
 							alt="instagram"
@@ -74,9 +74,9 @@ const Footer = () => {
 					</div>
 				</div>
 			</div>
-			<div className="px-4 py-10 space-y-4 md:space-y-6">
+			<div className="px-4 py-10 md:px-10 space-y-4 md:space-y-6 xl:!border-t-0">
 				<div className="space-y-4">
-					<h2 className="text-mobileHeading5 text-gray">Shop</h2>
+					<h2 className="text-mobileHeading5 text-gray-dark">Shop</h2>
 					<ul className="space-y-2 text-mobileLink md:text-desktopLink">
 						<li className="">All Products</li>
 						<li className="">Fresh Flowers</li>
@@ -88,16 +88,16 @@ const Footer = () => {
 					</ul>
 				</div>
 				<div className="space-y-4">
-					<h2 className="text-mobileHeading5 text-gray">Service</h2>
+					<h2 className="text-mobileHeading5 text-gray-dark">Service</h2>
 					<ul className="space-y-2 text-mobileLink md:text-desktopLink">
 						<li className="">Flower Subscription</li>
 						<li className="">Wedding & Event Decor</li>
 					</ul>
 				</div>
 			</div>
-			<div className="px-4 py-10 space-y-4 md:space-y-6">
+			<div className="px-4 py-10 md:px-10 space-y-4 md:space-y-6 xl:!border-t-0">
 				<div className="space-y-4">
-					<h2 className="text-mobileHeading5 text-gray">About Us</h2>
+					<h2 className="text-mobileHeading5 text-gray-dark">About Us</h2>
 					<ul className="space-y-2 text-mobileLink md:text-desktopLink">
 						<li className="">Our Story</li>
 						<li className="">
