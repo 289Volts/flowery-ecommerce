3946e8b9194bdd71cf9e4e289c4c1044a631c577
289volts <289volts@gmail.com>
2024-01-27 23:44:01 +0100

    Chore
    Fixed header by making it sticky to the top. Fixed menu drawer z-index so it is on top of the R mark. Added visual feedback for when user hovers on the card link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,208 +1,184 @@
-'use client';
+"use client";
 
-import cartIcon from '@/public/assets/icons/cart.svg';
-import close from '@/public/assets/icons/close.svg';
-import facebook from '@/public/assets/icons/facebook.svg';
-import menu from '@/public/assets/icons/ham-menu.svg';
-import instagram from '@/public/assets/icons/instagram.svg';
-import pinterest from '@/public/assets/icons/pinterest.svg';
-import telegram from '@/public/assets/icons/telegram.svg';
-import twitter from '@/public/assets/icons/twitter.svg';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
-import Button from '../ui/Button';
+import cartIcon from "@/public/assets/icons/cart.svg";
+import close from "@/public/assets/icons/close.svg";
+import facebook from "@/public/assets/icons/facebook.svg";
+import menu from "@/public/assets/icons/ham-menu.svg";
+import instagram from "@/public/assets/icons/instagram.svg";
+import pinterest from "@/public/assets/icons/pinterest.svg";
+import telegram from "@/public/assets/icons/telegram.svg";
+import twitter from "@/public/assets/icons/twitter.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import Button from "../ui/Button";
 
 const Header = () => {
-	const [open, setOpen] = useState(false);
-	const handleOpenMenu = () => {
-		setOpen(!open);
-	};
-	return (
-		<header className="">
-			<div className="desktop hidden lg:grid grid-cols-[1fr_1fr] border-y border-y-black">
-				<div className="flex">
-					<Link
-						href=""
-						className="w-max lg:w-[150px] lg:hover:bg-primary lg:hover:text-white active:bg-white active:text-primary  hover:transition-[background] hover:duration-300 xl:w-[180px] p-7 text-center"
-					>
-						Shop
-					</Link>
-					<Link
-						href=""
-						className="w-max lg:w-[150px] lg:hover:bg-primary lg:hover:text-white active:bg-white active:text-primary  hover:transition-[background] hover:duration-300 xl:w-[180px] p-7 border-x border-x-black text-center"
-					>
-						Contact
-					</Link>
-				</div>
-				<div className="flex col-end-4">
-					<Link
-						href=""
-						className="w-max lg:w-[150px] lg:hover:bg-primary lg:hover:text-white active:bg-white active:text-primary  hover:transition-[background] hover:duration-300 xl:w-[180px] p-7 border-x border-x-black text-center"
-					>
-						Sign in
-					</Link>
-					<Button
-						className="w-max lg:w-[150px] lg:hover:bg-primary lg:hover:text-white active:bg-white active:text-primary  hover:transition-[background] hover:duration-300 xl:w-[180px] border-y-0 p-7 capitalize border-l-0 justify-center !font-normal"
-						variant="secondary"
-					>
-						Cart
-					</Button>
-				</div>
-			</div>
-			<div className="mobile grid grid-cols-[1fr_1fr] lg:hidden border-y border-y-black ">
-				<Button
-					buttonProps={{
-						onClick: handleOpenMenu,
-						'aria-expanded': open ? 'true' : 'false',
-						'aria-label': 'open menu'
-					}}
-					className="w-max border-y-0 p-4"
-					variant="secondary"
-				>
-					<Image
-						src={menu as string}
-						alt=""
-						className=""
-					/>
-				</Button>
-				<Button
-					className="w-max col-end-4 border-y-0 p-4"
-					variant="secondary"
-				>
-					<Image
-						src={cartIcon as string}
-						alt=""
-						className=""
-					/>
-				</Button>
-				<div
-					className={`drawer absolute md:grid-cols-2 inset-0  transition-transform duration-500 grid ${
-						!open ? '-translate-x-[110%]' : 'translate-x-0 drawer-open'
-					}`}
-				>
-					<div className="flex flex-col bg-white border border-black">
-						<Button
-							buttonProps={{
-								onClick: handleOpenMenu,
-								'aria-label': 'close menu'
-							}}
-							className="w-max col-end-4 border-y-0 p-4"
-						>
-							<Image
-								src={close as string}
-								alt=""
-								className=""
-							/>
-						</Button>
-						<nav className="">
-							<ul className="flex flex-col">
-								<li className="">
-									<Link
-										onClick={handleOpenMenu}
-										href=""
-										className="p-5 border-t border-t-black flex w-full"
-									>
-										Sign in
-									</Link>
-								</li>
-								<li className="">
-									<Link
-										onClick={handleOpenMenu}
-										href=""
-										className="p-5 border-t border-t-black flex w-full"
-									>
-										Shop
-									</Link>
-								</li>
-								<li className="">
-									<span
-										onClick={handleOpenMenu}
-										className="p-5 border-t border-t-black flex w-full"
-									>
-										Services
-									</span>
-								</li>
-								<li className="">
-									<Link
-										onClick={handleOpenMenu}
-										href=""
-										className="p-5 border-t border-t-black flex w-full"
-									>
-										Contact
-									</Link>
-								</li>
-								<li className="">
-									<Link
-										onClick={handleOpenMenu}
-										href=""
-										className="p-5 border-y border-y-black flex w-full"
-									>
-										About us
-									</Link>
-								</li>
-								<li className="p-5 flex flex-col gap-4">
-									<span
-										onClick={handleOpenMenu}
-										className="w-full"
-									>
-										Shipping & returns
-									</span>
-									<span
-										onClick={handleOpenMenu}
-										className="w-full"
-									>
-										Terms & condition
-									</span>
-									<span
-										onClick={handleOpenMenu}
-										className="w-full"
-									>
-										Privacy policy
-									</span>
-								</li>
-							</ul>
-						</nav>
-						<div className="p-5 flex items-center justify-between mt-auto border-y border-y-black">
-							<Image
-								onClick={handleOpenMenu}
-								src={instagram as string}
-								alt="instagram"
-								className=""
-							/>
-							<Image
-								onClick={handleOpenMenu}
-								src={pinterest as string}
-								alt="pinterest"
-								className=""
-							/>
-							<Image
-								onClick={handleOpenMenu}
-								src={facebook as string}
-								alt="facebook"
-								className=""
-							/>
-							<Image
-								onClick={handleOpenMenu}
-								src={twitter as string}
-								alt="twitter"
-								className=""
-							/>
-							<Image
-								onClick={handleOpenMenu}
-								src={telegram as strings}
-								alt="telegram"
-								className=""
-							/>
-						</div>
-					</div>
-					<div
-						onClick={handleOpenMenu}
-						className={`bg-[rgba(210, 210, 215, 0.35)] backdrop-blur-sm hidden md:block`}
-					/>
-				</div>
-			</div>
-		</header>
-	);
+  const [open, setOpen] = useState(false);
+  const handleOpenMenu = () => {
+    setOpen(!open);
+  };
+  return (
+    <header className="bg-white sticky top-0">
+      <div className="desktop hidden lg:grid grid-cols-[1fr_1fr] border-y border-y-black">
+        <div className="flex">
+          <Link
+            href=""
+            className="w-max lg:w-[150px] lg:hover:bg-primary lg:hover:text-white active:bg-white active:text-primary  hover:transition-[background] hover:duration-300 xl:w-[180px] p-7 text-center"
+          >
+            Shop
+          </Link>
+          <Link
+            href=""
+            className="w-max lg:w-[150px] lg:hover:bg-primary lg:hover:text-white active:bg-white active:text-primary  hover:transition-[background] hover:duration-300 xl:w-[180px] p-7 border-x border-x-black text-center"
+          >
+            Contact
+          </Link>
+        </div>
+        <div className="flex col-end-4">
+          <Link
+            href=""
+            className="w-max lg:w-[150px] lg:hover:bg-primary lg:hover:text-white active:bg-white active:text-primary  hover:transition-[background] hover:duration-300 xl:w-[180px] p-7 border-x border-x-black text-center"
+          >
+            Sign in
+          </Link>
+          <Button
+            className="w-max lg:w-[150px] lg:hover:bg-primary lg:hover:text-white active:bg-white active:text-primary  hover:transition-[background] hover:duration-300 xl:w-[180px] border-y-0 p-7 capitalize border-l-0 justify-center !font-normal"
+            variant="secondary"
+          >
+            Cart
+          </Button>
+        </div>
+      </div>
+      <div className="mobile grid grid-cols-[1fr_1fr] lg:hidden border-y border-y-black ">
+        <Button
+          buttonProps={{
+            onClick: handleOpenMenu,
+            "aria-expanded": open ? "true" : "false",
+            "aria-label": "open menu",
+          }}
+          className="w-max border-y-0 p-4"
+          variant="secondary"
+        >
+          <Image src={menu as string} alt="" className="" />
+        </Button>
+        <Button className="w-max col-end-4 border-y-0 p-4" variant="secondary">
+          <Image src={cartIcon as string} alt="" className="" />
+        </Button>
+        <div
+          className={`drawer fixed sm:grid-cols-2 inset-0  transition-transform duration-500 grid ${
+            !open ? "-translate-x-[110%]" : "translate-x-0 drawer-open z-10"
+          }`}
+        >
+          <div className="flex flex-col bg-white border border-black">
+            <Button
+              buttonProps={{
+                onClick: handleOpenMenu,
+                "aria-label": "close menu",
+              }}
+              className="w-max col-end-4 border-y-0 p-4"
+            >
+              <Image src={close as string} alt="" className="" />
+            </Button>
+            <nav className="">
+              <ul className="flex flex-col">
+                <li className="">
+                  <Link
+                    onClick={handleOpenMenu}
+                    href=""
+                    className="p-5 border-t border-t-black flex w-full"
+                  >
+                    Sign in
+                  </Link>
+                </li>
+                <li className="">
+                  <Link
+                    onClick={handleOpenMenu}
+                    href=""
+                    className="p-5 border-t border-t-black flex w-full"
+                  >
+                    Shop
+                  </Link>
+                </li>
+                <li className="">
+                  <span
+                    onClick={handleOpenMenu}
+                    className="p-5 border-t border-t-black flex w-full"
+                  >
+                    Services
+                  </span>
+                </li>
+                <li className="">
+                  <Link
+                    onClick={handleOpenMenu}
+                    href=""
+                    className="p-5 border-t border-t-black flex w-full"
+                  >
+                    Contact
+                  </Link>
+                </li>
+                <li className="">
+                  <Link
+                    onClick={handleOpenMenu}
+                    href=""
+                    className="p-5 border-y border-y-black flex w-full"
+                  >
+                    About us
+                  </Link>
+                </li>
+                <li className="p-5 flex flex-col gap-4">
+                  <span onClick={handleOpenMenu} className="w-full">
+                    Shipping & returns
+                  </span>
+                  <span onClick={handleOpenMenu} className="w-full">
+                    Terms & condition
+                  </span>
+                  <span onClick={handleOpenMenu} className="w-full">
+                    Privacy policy
+                  </span>
+                </li>
+              </ul>
+            </nav>
+            <div className="p-5 flex items-center justify-between mt-auto border-y border-y-black">
+              <Image
+                onClick={handleOpenMenu}
+                src={instagram as string}
+                alt="instagram"
+                className=""
+              />
+              <Image
+                onClick={handleOpenMenu}
+                src={pinterest as string}
+                alt="pinterest"
+                className=""
+              />
+              <Image
+                onClick={handleOpenMenu}
+                src={facebook as string}
+                alt="facebook"
+                className=""
+              />
+              <Image
+                onClick={handleOpenMenu}
+                src={twitter as string}
+                alt="twitter"
+                className=""
+              />
+              <Image
+                onClick={handleOpenMenu}
+                src={telegram as string}
+                alt="telegram"
+                className=""
+              />
+            </div>
+          </div>
+          <div
+            onClick={handleOpenMenu}
+            className={`bg-[rgba(210, 210, 215, 0.35)] backdrop-blur-sm hidden sm:block`}
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 export default Header;
--- src/components/shared/CardLink.tsx
+++ src/components/shared/CardLink.tsx
@@ -19,14 +19,14 @@ const CardLink = ({
     <Link
       href={href}
       className={cn(
-        "flex min-h-[200px] flex-col items-center justify-center p-3 text-center text-primary md:p-6",
+        "flex min-h-[200px] flex-col items-center justify-center p-3 text-center text-primary md:p-6 group",
         className,
       )}
     >
       <span className="my-auto text-mobileHeading3 capitalize md:text-desktopHeading3">
         {textContent}
       </span>
-      <div className="flex items-center gap-1 text-sm font-semibold leading-[120%] md:text-base">
+      <div className="flex items-center gap-1 text-sm font-semibold leading-[120%] md:text-base group-focus-visible:underline group-focus-visible:underline-offset-4">
         <span className="">{linkText}</span>
         <Image src={arrRight as string} alt="" className="" />
       </div>
