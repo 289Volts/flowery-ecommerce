c8295fc6e4ac1ecfa0a66e345d86814e54041821
289volts <289volts@gmail.com>
2024-01-28 01:13:06 +0100

    Feat
    About us section tablet view done

--- src/components/Home/AboutSection.tsx
+++ src/components/Home/AboutSection.tsx
@@ -3,17 +3,26 @@ import Action from "../ui/Action";
 
 const AboutSection = ({ data }: TAboutSectionProps) => {
   return (
-    <section className="grid sm:grid-cols-2">
-      <div className="py-10 px-4">
-        <h2 className="text-mobileHeading2 uppercase">about us</h2>
+    <section className="grid lg:grid-cols-2 text-primary">
+      <div className="py-10 px-4 sm:p-20">
+        <h2 className="text-mobileHeading2 uppercase sm:text-desktopHeading2">
+          about us
+        </h2>
       </div>
-      <div className="py-10 px-4 space-y-16">
-        <div className="border-t border-t-primary">
+      <div className="py-10 px-4 sm:p-20 space-y-16 border-t border-t-primary">
+        <div className="">
           <h4 className="uppercase text-mobileOverline mb-6">our story</h4>
-          <h3 className="text-mobileHeading3 mb-4">{data.heading}</h3>
+          <h3 className="text-mobileHeading3 mb-4 sm:text-desktopHeading3">
+            {data.heading}
+          </h3>
           <p className="text-mobileBody text-pretty">{data.paragraph}</p>
         </div>
-        <Action variant="secondary" isLink className="uppercase" href="/about">
+        <Action
+          variant="secondary"
+          isLink
+          className="uppercase sm:w-fit"
+          href="/about"
+        >
           learn more
         </Action>
       </div>
