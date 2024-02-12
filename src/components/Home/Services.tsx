d4e78364086990d458da0d4ae8e6752105580b83
Roland Joshua <289volts@gmail.com>
2024-02-12 19:19:32 +0000

    Style
    Completed tablet and desktop styling for services first section

--- src/components/Home/Services.tsx
+++ src/components/Home/Services.tsx
@@ -9,9 +9,9 @@ const Services = ({ data }: TServicesSectionProps) => {
   //  const serviceImg = heroImgOpt.url();
   // console.log();
   return (
-    <section className="">
+    <section className="md:text-center">
       <SectionHeading heading="our service" />
-      <div className="border-t border-t-primary">
+      <div className="border-t border-t-primary text-left ">
         {data.map(
           (
             {
@@ -26,19 +26,19 @@ const Services = ({ data }: TServicesSectionProps) => {
             const serviceImgOpt = urlFor(imageUrl);
             const serviceImg = serviceImgOpt.url();
             return (
-              <div className="relative" key={heading}>
+              <div className="relative lg:grid lg:grid-cols-2" key={heading}>
                 {i === 1 && (
                   <div className="absolute inset-0 bg-black/40 z-[-1]" />
                 )}
                 <Image
                   src={"" + serviceImg}
-                  className={`w-full h-full ${i === 1 ? "absolute inset-0 object-cover z-[-2]" : ""}`}
+                  className={`w-full h-full md:max-h-[500px] lg:max-h-full object-cover object-top ${i === 1 ? "absolute inset-0 object-cover z-[-2]" : ""}`}
                   alt=""
                   width={serviceImgOpt.width(360).options.width}
                   height={serviceImgOpt.height(360).options.height}
                 />
                 <div
-                  className={`text-center py-10 sm:p-20 px-4 border-t border-t-primary ${i === 1 ? "text-white py-[3.2rem]" : ""}`}
+                  className={`text-center py-10 sm:p-20 px-4 border-t border-t-primary lg:flex lg:flex-col lg:justify-center lg:items-center ${i === 1 ? "text-white py-[3.2rem]" : ""}`}
                 >
                   <h4 className="text-mobileOverline mb-6 sm:text-desktopOverline uppercase">
                     service
@@ -53,7 +53,7 @@ const Services = ({ data }: TServicesSectionProps) => {
                     isLink
                     href="/subscriptions"
                     variant={i === 1 ? "tertiary" : "secondary"}
-                    className="uppercase"
+                    className="uppercase lg:w-fit"
                   >
                     subscribe now
                   </Action>
